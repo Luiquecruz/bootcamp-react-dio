6093f29faeda00d3a87ad04cb784ff642a047962
luiquecruz <luiquecruz@gmail.com>
2024-04-19 01:32:40 -0300

    add layout max width

--- github-perfil/src/App.jsx
+++ github-perfil/src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <main>
+      <main className="max-w-xl mx-auto">
         <Home />
       </main>
     </>
--- github-perfil/src/Components/Header/index.jsx
+++ github-perfil/src/Components/Header/index.jsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <header className="bg-stone-900 p-4 rounded-full">
+    <header className="bg-stone-900 p-4 rounded-full max-w-xl mx-auto">
       <h1 className="text-3xl text-white text-center font-bold">Github Perfil</h1>
     </header>
   )
