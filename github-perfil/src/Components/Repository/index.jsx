fa9bfe942f7ea57470f74217ca34beed8cc3cf94
luiquecruz <luiquecruz@gmail.com>
2024-04-19 23:39:03 -0300

    update data prop to receive an object

--- github-perfil/src/Components/Repository/index.jsx
+++ github-perfil/src/Components/Repository/index.jsx
@@ -1,9 +1,11 @@
-const Repository = ({url, title, description}) => {
+const Repository = ({data}) => {
   return (
+    // @TODO: add star icon (filled | outlined) to "star a repo"
     <li className="bg-stone-900 rounded-2xl hover:bg-stone-800 transition">
-      <a href={url} target="_blank" rel="noopener noreferrer" className="block p-4">
-        <strong className="block text-lg text-blue-500">{title}</strong>
-        <p className="text-sm text-stone-400">{description}</p>
+      <a href={data?.html_url} target="_blank" rel="noopener noreferrer" className="block p-4">
+        <strong className="block leading-none text-lg text-blue-500">{data?.name}</strong>
+        <small className="block text-stone-500 mt-1">{data?.full_name}</small>
+        {data?.description && (<p className="text-sm text-stone-400 mt-2">{data?.description}</p>)}
       </a>
     </li>
   )
